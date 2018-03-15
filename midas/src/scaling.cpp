8c72b003b63cc14acc52d3d32f4f1101ec6032b0
jlorenz <jlorenz@hive.informatik.tu-cottbus.de>
2018-03-15 18:58:18 +0100

    Midas/Scaling: improvements
    + improved tracking
    + added better thread-wise evaluation
    + added total evaluation
    + added more debug code

--- midas/src/scaling.cpp
+++ midas/src/scaling.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>// std::min_element, std::max_element
 #include <random>   // std::random_device, std::uniform_int_distribution
 #include <pthread.h>
+#include <sstream>
 
 #include "utils.hpp"
 #include "midas.hpp"
@@ -26,13 +27,15 @@ struct BenchThreadResult {
     std::size_t num_failures = 0;
     std::size_t num_rw_conflicts = 0;
     std::size_t num_ww_conflicts = 0;
-    std::size_t num_key_misses = 0;
+    std::size_t num_r_snapshot_misses = 0;
+    std::size_t num_w_snapshot_misses = 0;
     std::size_t num_invalid_txs = 0;
     std::chrono::high_resolution_clock::time_point start;
     std::chrono::high_resolution_clock::time_point end;
 };
 
 struct BenchThreadArgs {
+    unsigned id;
     cpu_set_t cpu_set;
     ProgramArgs* pargs;
     midas::Store* store;
@@ -41,15 +44,40 @@ struct BenchThreadArgs {
     BenchThreadResult result;
 };
 
+std::string print_midas_ret(unsigned code)
+{
+    switch (code) {
+    case midas::Store::OK:
+        return "OK";
+
+    case midas::Store::INVALID_TX:
+        return "INVALID_TX";
+
+    case midas::Store::RW_CONFLICT:
+        return "RW_CONFLICT";
+
+    case midas::Store::WW_CONFLICT:
+        return "WW_CONFLICT";
+
+    case midas::Store::VALUE_NOT_FOUND:
+        return "VALUE_NOT_FOUND";
+
+    default:
+        return "UNKNOWN STATUS CODE";
+    }
+}
+
 void* worker_routine(void* arg)
 {
     BenchThreadArgs* worker_args = (BenchThreadArgs *) arg;
     ProgramArgs* prog_args = worker_args->pargs;
 
+    // auto pid = pthread_self();
+    // auto pid = worker_args->id;
+
     const auto store = worker_args->store;
     const auto pairs = worker_args->pairs;
     const auto num_txs = prog_args->num_txs;
-    // const auto num_retries_max = prog_args->num_retries;
     const auto tx_profiles = worker_args->tx_profiles;
     const auto tx_len_min = prog_args->tx_len_min;
     const auto tx_len_max = prog_args->tx_len_max;
@@ -81,8 +109,11 @@ void* worker_routine(void* arg)
     std::size_t num_failures = 0;
     std::size_t num_rw_conflicts = 0;
     std::size_t num_ww_conflicts = 0;
-    std::size_t num_key_misses = 0;
+    std::size_t num_r_snapshot_misses = 0;
+    std::size_t num_w_snapshot_misses = 0;
     std::size_t num_invalid_txs = 0;
+
+    // std::stringstream ss;
 
     // ########################################################################
     // ## START ###############################################################
@@ -101,12 +132,22 @@ void* worker_routine(void* arg)
             rand -= p->prob;
         }
 
-        std::cout << "selected profile: " << prof->name << std::endl;
+        // std::cout << pid << "> selected profile: " << prof->name << std::endl;
+        // ss << "[" << pid << "] selected profile: " << prof->name << std::endl;
+        // ss << "[" << pid << ": tx=" << i << '/' << num_txs << "] ";
+        // ss << "selected profile: " << prof->name << std::endl;
+        // std::cout << ss.str();
+        // ss.str("");
 
         // select random tx length (using normal distribution based on profile)
         auto tx_length = std::round(len_dist(rng));
 
-        std::cout << "selected tx length: " << tx_length << std::endl;
+        // std::cout << pid << "> selected tx length: " << tx_length << std::endl;
+        // ss << "[" << pid << "] selected tx length: " << tx_length << std::endl;
+        // ss << "[" << pid << ": tx=" << i << '/' << num_txs << "] ";
+        // ss << "selected tx length: " << tx_length << std::endl;
+        // std::cout << ss.str();
+        // ss.str("");
 
         // begin transaction
         auto tx = store->begin();
@@ -123,29 +164,80 @@ void* worker_routine(void* arg)
                 rand -= prob;
             }
 
-            std::cout << "selected operation: " << opcode << std::endl;
+            // std::cout << pid << "> selected operation: " << opcode << std::endl;
+            // ss << "[" << pid << "] selected operation: " << opcode << std::endl;
+            // ss << "[" << pid << ": tx=" << i << '/' << num_txs;
+            // ss << ", op=" << step << '/' << tx_length;
+            // ss << " | id=" << tx->getId();
+            // ss << ", ts=" << tx->getBegin() << "] ";
+            // ss << "selected operation: " << opcode << std::endl;
+            // std::cout << ss.str();
+            // ss.str("");
 
             // select random pair (using uniform distribution)
             const auto& [key, val] = (*pairs)[pair_dist(rng)];
 
-            std::cout << "selected pair: [" << key << ", " << val << "]" << std::endl;
+            // std::cout << pid << "> selected pair: [" << key << ", " << val << "]" << std::endl;
+            // ss << "[" << pid << "] selected pair: [" << key << ", " << val << "]" << std::endl;
+            // ss << "[" << pid << ": tx=" << i << '/' << num_txs;
+            // ss << ", op=" << step << '/' << tx_length;
+            // ss << " | id=" << tx->getId();
+            // ss << ", ts=" << tx->getBegin() << "] ";
+            // ss << "selected pair: [" << key << ", " << val << "]" << std::endl;
+            // std::cout << ss.str();
+            // ss.str("");
 
             // perform operation
             switch (opcode) {
             case OpCode::Get:
-                store->read(tx, key, result);
+                if (auto ret = store->read(tx, key, result); ret != midas::Store::OK) {
+                    if (ret == midas::Store::VALUE_NOT_FOUND)
+                        ++num_r_snapshot_misses;
+                    // ss << "[" << pid << "] Get -> " << print_midas_ret(ret);
+                    // ss << "[" << pid << ": tx=" << i << '/' << num_txs;
+                    // ss << ", op=" << step << '/' << tx_length;
+                    // ss << " | id=" << tx->getId();
+                    // ss << ", ts=" << tx->getBegin() << "] ";
+                    // ss << "Get -> " << print_midas_ret(ret);
+                    // std::cout << ss.str() << std::endl;
+                    // ss.str("");
+                    // exit(0);
+                }
                 break;
 
             case OpCode::Put:
-                store->write(tx, key, val);
+                if (auto ret = store->write(tx, key, val); ret != midas::Store::OK) {
+                    if (ret == midas::Store::VALUE_NOT_FOUND)
+                        ++num_w_snapshot_misses;
+                    // ss << "[" << pid << ": tx=" << i << '/' << num_txs;
+                    // ss << ", op=" << step << '/' << tx_length;
+                    // ss << " | id=" << tx->getId();
+                    // ss << ", ts=" << tx->getBegin() << "] ";
+                    // ss << "Put -> " << print_midas_ret(ret);
+                    // std::cout << ss.str() << std::endl;
+                    // std::cerr << ss.str() << std::endl;
+                    // ss.str("");
+                    // store->print();
+                    // exit(0);
+                }
                 break;
 
             case OpCode::Ins:
-                store->write(tx, key, val);
+                if (auto ret = store->write(tx, key, val); ret != midas::Store::OK) {
+                    // ss << "[" << pid << "] Ins -> " << print_midas_ret(ret);
+                    // std::cout << ss.str() << std::endl;
+                    // ss.str("");
+                    // exit(0);
+                }
                 break;
 
             case OpCode::Del:
-                store->drop(tx, key);
+                if (auto ret = store->drop(tx, key); ret != midas::Store::OK) {
+                    // ss << "[" << pid << "] Del -> " << print_midas_ret(ret);
+                    // std::cout << ss.str() << std::endl;
+                    // ss.str("");
+                    // exit(0);
+                }
                 break;
 
             default:
@@ -153,20 +245,25 @@ void* worker_routine(void* arg)
             }
         }
 
+        if (tx->getStatus().load() == midas::Transaction::FAILED) {
+            ++num_failures;
+            continue;
+        }
+
         // commit transaction; increase error counters if necessary
         const auto status = store->commit(tx);
+        // ss << "[" << pid << "] commit -> " << print_midas_ret(status) << std::endl;
+        // std::cout << ss.str();
+        // ss.str("");
         if (status != midas::Store::OK) {
             ++num_failures;
             if (status == midas::Store::WW_CONFLICT)
                 ++num_ww_conflicts;
             else if (status == midas::Store::RW_CONFLICT)
                 ++num_rw_conflicts;
-            else if (status == midas::Store::VALUE_NOT_FOUND)
-                ++num_key_misses;
-            else if (status == midas::Store::INVALID_TX)
+            else if (status == midas::Store::INVALID_TX) {
                 ++num_invalid_txs;
-
-            // TODO make sure this tx is retried (do we need that?)
+            }
         }
     }
 
@@ -176,16 +273,11 @@ void* worker_routine(void* arg)
     // ## END #################################################################
     // ########################################################################
 
-    // std::cout << "time elapsed = " << convert_duration(time_end - time_start, time_unit) << time_unit << std::endl;
-    // std::cout << "#failures = " << num_failures << std::endl;
-    // std::cout << "#misses = " << num_misses << std::endl;
-    // std::cout << "#w/w conflicts = " << num_ww_conflicts << std::endl;
-    // std::cout << "#r/w conflicts = " << num_rw_conflicts << std::endl;
-
     worker_args->result.num_failures = num_failures;
     worker_args->result.num_rw_conflicts = num_rw_conflicts;
     worker_args->result.num_ww_conflicts = num_ww_conflicts;
-    worker_args->result.num_key_misses = num_key_misses;
+    worker_args->result.num_r_snapshot_misses = num_r_snapshot_misses;
+    worker_args->result.num_w_snapshot_misses = num_w_snapshot_misses;
     worker_args->result.num_invalid_txs = num_invalid_txs;
     worker_args->result.start = time_start;
     worker_args->result.end = time_end;
@@ -246,6 +338,7 @@ int run(ProgramArgs* pargs)
         thread_args[i].store = &store;
         thread_args[i].pairs = &pairs;
         thread_args[i].tx_profiles = &profiles;
+        thread_args[i].id = i;
 
         /* Create Attributes */
         rc = pthread_attr_init(&attr);
@@ -281,14 +374,35 @@ int run(ProgramArgs* pargs)
             std::printf("pthread_join() returned error=%d\n", rc);
     }
 
+    const auto time_unit = pargs->unit;
     std::size_t num_failures = 0;
     std::size_t num_rw_conflicts = 0;
     std::size_t num_ww_conflicts = 0;
-    std::size_t num_key_misses = 0;
+    std::size_t num_r_snapshot_misses = 0;
+    std::size_t num_w_snapshot_misses = 0;
     std::size_t num_invalid_txs = 0;
     std::chrono::high_resolution_clock::time_point glob_start;
     std::chrono::high_resolution_clock::time_point glob_end;
     for (std::size_t i=0; i<pargs->num_threads; ++i) {
+        std::cout << "----------------------------------------\n";
+        std::cout << "results for thread-" << i << '\n';
+        std::cout << "----------------------------------------\n";
+        std::cout << "#failures      = " << thread_args[i].result.num_failures << std::endl;
+        std::cout << "#r snap misses = " << thread_args[i].result.num_r_snapshot_misses << std::endl;
+        std::cout << "#w snap misses = " << thread_args[i].result.num_w_snapshot_misses << std::endl;
+        // std::cout << "#invalid txs   = " << thread_args[i].result.num_invalid_txs << std::endl;
+        std::cout << "#w/w conflicts = " << (thread_args[i].result.num_ww_conflicts + thread_args[i].result.num_w_snapshot_misses) << std::endl;
+        std::cout << "#r/w conflicts = " << thread_args[i].result.num_rw_conflicts << std::endl;
+        std::cout << "duration       = " << convert_duration(
+                thread_args[i].result.end - thread_args[i].result.start,
+                time_unit) << time_unit << std::endl;
+
+        num_failures += thread_args[i].result.num_failures;
+        num_rw_conflicts += thread_args[i].result.num_rw_conflicts;
+        num_ww_conflicts += thread_args[i].result.num_ww_conflicts;
+        num_r_snapshot_misses += thread_args[i].result.num_r_snapshot_misses;
+        num_w_snapshot_misses += thread_args[i].result.num_w_snapshot_misses;
+        num_invalid_txs += thread_args[i].result.num_invalid_txs;
         if (i == 0) {
             glob_start = thread_args[i].result.start;
             glob_end = thread_args[i].result.end;
@@ -299,21 +413,18 @@ int run(ProgramArgs* pargs)
             if (thread_args[i].result.end > glob_end)
                 glob_end = thread_args[i].result.end;
         }
-        num_failures += thread_args[i].result.num_failures;
-        num_rw_conflicts += thread_args[i].result.num_rw_conflicts;
-        num_ww_conflicts += thread_args[i].result.num_ww_conflicts;
-        num_key_misses += thread_args[i].result.num_key_misses;
-        num_invalid_txs += thread_args[i].result.num_invalid_txs;
     }
 
-    const auto time_unit = pargs->unit;
-    std::cout << "total time = " << convert_duration(glob_end - glob_start, time_unit) << time_unit << std::endl;
-    std::cout << "total #failures = " << num_failures << std::endl;
-    std::cout << "total #misses = " << num_key_misses << std::endl;
-    std::cout << "total #invalid txs = " << num_invalid_txs << std::endl;
-    std::cout << "total #w/w conflicts = " << num_ww_conflicts << std::endl;
+    std::cout << "----------------------------------------\n";
+    std::cout << "summary" << '\n';
+    std::cout << "----------------------------------------\n";
+    std::cout << "total time           = " << convert_duration(glob_end - glob_start, time_unit) << time_unit << std::endl;
+    std::cout << "total #failures      = " << num_failures << std::endl;
+    std::cout << "total #r snap misses = " << num_r_snapshot_misses << std::endl;
+    std::cout << "total #w snap misses = " << num_w_snapshot_misses << std::endl;
+    std::cout << "total #invalid txs   = " << num_invalid_txs << std::endl;
+    std::cout << "total #w/w conflicts = " << (num_ww_conflicts + num_w_snapshot_misses) << std::endl;
     std::cout << "total #r/w conflicts = " << num_rw_conflicts << std::endl;
-
 
     // ########################################################################
     // Cleanup
