2ba8e24e9a7077172867cfbb1ed511aef287ed03
jlorenz <jlorenz@hive.informatik.tu-cottbus.de>
2018-03-20 00:06:43 +0100

    Added scripts + untracked data

---
+++ scripts/run-scale-all-3M.sh
@@ -0,0 +1,4 @@
+rm -f /dev/shm/nvdimm_midas && ./midas/bin/scaling -d data/dat-16-32-65536.csv -p mixed-sap-oltp.cfg -p read-only.cfg -a 64 -t 1 -n 1000
+rm -f /dev/shm/nvdimm_midas && ./midas/bin/scaling -d data/dat-16-32-65536.csv -p mixed-sap-oltp.cfg -p read-only.cfg -a 64 -t 2 -n 500
+rm -f /dev/shm/nvdimm_midas && ./midas/bin/scaling -d data/dat-16-32-65536.csv -p mixed-sap-oltp.cfg -p read-only.cfg -a 64 -t 4 -n 250
+rm -f /dev/shm/nvdimm_midas && ./midas/bin/scaling -d data/dat-16-32-65536.csv -p mixed-sap-oltp.cfg -p read-only.cfg -a 64 -t 8 -n 125
