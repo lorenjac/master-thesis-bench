c84b4ecd13117a1711f348b657cad47279beab98
Jacob Lorenz <jacob.lorenz@b-tu.de>
2018-04-02 12:59:26 +0200

    Script refactoring
    + added top level script to run all benchmarks
    + replaced constants in previous midas-run script with parameters
    + some benchmark outputs (midas + echo) now require verbose mode
    + fixed some misaligned result outputs in echo scaling benchmark

--- scripts/run-midas.sh
+++ scripts/run-midas.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-data_file=assets/data/medium-128-1024-65536.csv
-workload_file=assets/workloads/medium-oltp-2-64-1000-1.json
-num_threads_max=32
+data_file=$1
+workload_file=$2
+num_threads_max=$3
+num_runs=$4
 
-num_runs=$1
-
-echo "dataset: $data_file"
-echo "workload: $workload_file"
-echo "num_threads_max: $num_threads_max"
-echo "#runs per config: $num_runs"
+# echo "dataset: $data_file"
+# echo "workload: $workload_file"
+# echo "num_threads_max: $num_threads_max"
+# echo "#runs per config: $num_runs"
 
 for ((n=1; n<=$num_threads_max; n=2*n))
 do
@@ -17,6 +16,8 @@ do
     for ((i=1; i<=$num_runs; i++))
     do
         echo -n "starting run $i/$num_runs ..."
+        echo "--------------------------------" >> log/midas-medium-$n.log
+        echo "num_threads=$n" >> log/midas-medium-$n.log
         rm -f /dev/shm/nvdimm_midas && PMEM_IS_PMEM_FORCE=1 ./bin/midas-scaling --data $data_file --workload $workload_file --num-threads $n >> log/midas-medium-$n.log
         echo "done!"
     done
--- scripts/run-scale-all-75M-long.sh
+++
@@ -1,4 +0,0 @@
-rm -f /dev/shm/nvdimm_midas && ./midas/bin/scaling -d data/dat-128-1024-65536.csv -p mixed-sap-oltp.cfg -p read-only.cfg -a 64 -t 1 -n 10000
-rm -f /dev/shm/nvdimm_midas && ./midas/bin/scaling -d data/dat-128-1024-65536.csv -p mixed-sap-oltp.cfg -p read-only.cfg -a 64 -t 2 -n 5000
-rm -f /dev/shm/nvdimm_midas && ./midas/bin/scaling -d data/dat-128-1024-65536.csv -p mixed-sap-oltp.cfg -p read-only.cfg -a 64 -t 4 -n 2500
-rm -f /dev/shm/nvdimm_midas && ./midas/bin/scaling -d data/dat-128-1024-65536.csv -p mixed-sap-oltp.cfg -p read-only.cfg -a 64 -t 8 -n 1250
